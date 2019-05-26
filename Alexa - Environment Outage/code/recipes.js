f25acad05f35c14430a0d2dbe9a65e9ecada9f81
munibabu.annachi <munibabu.annachi@accenture.com>
2019-05-26 10:21:38 -0400

    v1

---
+++ Alexa - Environment Outage/code/recipes.js
@@ -0,0 +1,12 @@
+/* eslint-disable  func-names */
+/* eslint-disable max-len */
+/* eslint quote-props: ['error', 'consistent'] */
+module.exports = {
+  'RECIPE_EN_US': {
+    'outage': 'there is no planned outage for today. but there is one for tomorrow. G C P tracker website will be down for one hour from 11PM to 12AM for scheduled maintainance activity.',
+    'unplanned outage': 'there was one in the morning 8:30 AM with my learning website. outage was for one hour till 9:30 AM.',
+    'my performance website': 'there in no problem is with my performance website, it is functioning well. You can submit an incident if you get into any problem accessing the website.',
+    'my learning website': 'Problem was identified in my learning site today morning 8:30 AM. Incident five fouur eight zero was raised for the same. Incident is in resolved status now and site is available. you can access it now, happy learning',
+ 
+  },
+};
---
+++ Alexa - Knowledge Base/code/recipes.js
@@ -0,0 +1,14 @@
+/* eslint-disable  func-names */
+/* eslint-disable max-len */
+/* eslint quote-props: ['error', 'consistent'] */
+module.exports = {
+  'RECIPE_EN_US': {
+    'firewall': 'Consult with DevOps',
+    'load balance': 'We can create load balance point by using various tools',
+    'jenkins': 'Jenkins is continuos integration tool which can be setup easily by pulling docker image',
+    'docker': 'docker can be setup and installed by following instructions online',
+    'git repo': 'open account on github and create a repo',
+    'pcf access': 'Consult with DevOps',
+    'grafana': 'Consult with DevOps',
+   },
+};
